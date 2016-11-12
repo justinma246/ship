92886e2d41bbd684e2058b904f6b505850493b52
Justin Ma <justinma246@gmail.com>
2016-11-12 15:24:36 -0800

    things

--- client/app/bundles/ship/containers/ProfilePage.jsx
+++ client/app/bundles/ship/containers/ProfilePage.jsx
@@ -10,10 +10,27 @@ export default class ProfilePage extends React.Component {
     let user = this.props.user
 
     return (
-      <div>
-        PROFILE PAGE
-
+      <div class="row">
+        <div class="col-md-4">
+          <div class="profile" id="user1">
+            <h1> Username: {user.name} </h1>
+            <img src={user.picture}/>
+            <h1>INFO</h1>
+            <h1>SETTINGS</h1>
+            <h1>HELP</h1>
+          </div>
+        </div>
+        <div class="col-md-4">
+          <div class="profile" id="user1">
+            <h1> Username: {user.name} </h1>
+            <img src={user.picture}/>
+            <h1>INFO</h1>
+            <h1>SETTINGS</h1>
+            <h1>HELP</h1>
+          </div>
+        </div>
       </div>
+      
     )
   }
 }
