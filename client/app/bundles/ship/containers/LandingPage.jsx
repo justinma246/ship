0363a662b2897dbb0ea0eafb11b6c814ad8f9822
Justin Ma <justinma246@gmail.com>
2016-11-12 19:17:48 -0800

    Fixed double form

--- client/app/bundles/ship/containers/LandingPage.jsx
+++ client/app/bundles/ship/containers/LandingPage.jsx
@@ -20,7 +20,9 @@ export default class LandingPage extends React.Component {
           <a className="button" href="/auth/facebook">Sign in with Facebook</a>
         </div>
         <div className="help" onMouseOver={()=>this.show('div1')} onMouseOut={()=>this.hide('div1')}>?</div>
-        <div id="div1">HELLO???</div>
+        <div id="div1">Forget the struggles of searching for your own soul mate - have other people find them for you!
+
+</div>
       </div>
     )
   }
